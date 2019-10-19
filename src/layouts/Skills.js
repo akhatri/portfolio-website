f03cb16426932615b801fe4e4a73fa21b847eb05
Akshat Khatri <akshatster@gmail.com>
2019-10-19 21:30:56 +0100

    content changes

--- src/layouts/Skills.js
+++ src/layouts/Skills.js
@@ -20,10 +20,10 @@ class Skills extends Component {
         <div className="container py-5">
           <h1>Skills</h1>
           <div>
-            <p>As the front-end landscape constantly evolves at break neck speed it's important for any developer to keep up with the latest trends and languages. I've started to focus on ES6+, React.js and Vue.js and I'm slowly moving away from jQuery in favour of vanilla javascript as most of jQuery's capabilities are now possible with ES6</p>
-            <p>On the back-end side of things, I have a fair understanding of the listing back-end technologies and who knows I might one day become a full-stack Unicorn :)</p>
+            <p>As the front-end landscape constantly evolves at breakneck speed it's important for any developer to keep up with the latest trends and languages. I've started to focus on ES6+, React.js and Vue.js and I'm slowly moving away from jQuery in favour of vanilla javascript as most of jQuery's capabilities are now possible with ES6</p>
+            <p>On the back-end side of things, I have a fair understanding of the listed back-end technologies and who knows I might one day become a full-stack Unicorn :)</p>
             <p>Although I have a good knowledge of Angular 1, LESS, SVN, Compass and Bower - I have moved on from these technologies to market trends</p>
-            <p>Below are my various skills with beginner competencies marked for certain technologies</p>
+            <p>Below are my various skills with competency levels marked for certain technologies</p>
           </div>
 
           <div className="card-container grid-wrapper col-3 grid-gap-2">
