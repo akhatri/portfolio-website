1a5f339ad31b2d8940a1c44f6ce8900c167a1033
Akshat Khatri <akshatster@gmail.com>
2019-10-21 17:40:47 +0100

    updates

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -18,9 +18,9 @@ class Contact extends Component {
           <div className="grid-wrapper col-2 align-center grid-gap-4">
             <img className="img-fluid rounded" src={ContactImage} alt="Get in touch" />
             <div>
-              <h1>Get in touch <span aria-labelledby="Emoji heart letter" role="img">💌</span></h1>
+              <h1>Get in touch <span aria-label="Emoji heart letter" role="img">💌</span></h1>
               <p>If you would like to get in touch regarding any freelance consulting projects and start-up advice on technology roadmaps, please get in touch via email <a href="mailto:akshatster@gmail.com">here</a></p>
-              <p>I'm currently based in UK and open to any contract roles too, depending on my work-load <span aria-labelledby="Emoji smiley" role="img">😄</span></p>
+              <p>I'm currently based in UK and open to any contract roles too, depending on my work-load <span aria-label="Emoji smiley" role="img">😄</span></p>
             </div>
           </div>
         </div>
