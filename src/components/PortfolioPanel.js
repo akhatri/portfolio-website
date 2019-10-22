6f70c9b40ca522c948af796559af15780e6eb46b
Akshat Khatri <akshatster@gmail.com>
2019-10-22 14:20:54 +0100

    fade at individual panels

--- src/components/PortfolioPanel.js
+++ src/components/PortfolioPanel.js
@@ -1,40 +1,43 @@
 import React from 'react';
-
 import '../styles/portfolio-panel.scss';
+
+// Libraries
+import Fade from 'react-reveal/Fade';
 
 const PortfolioPanel = (props) => {
 
   let { themeClass, name, image, imageWebP, company, description, multipleButtons, links, link, tags } = props.portfolioData;
 
   return (
-    <div className={`${themeClass} portfolio grid-wrapper col-2 align-center grid-gap-4 my-3`}>
-      <div className="media">
-        <picture>
-          <source srcSet={imageWebP} type="image/webp"/>
-          <source srcSet={image} type="image/png"/> 
-          <img className="img-fluid rounded media__image" src={image} alt={`${company}`}/>
-        </picture>
-      </div>
-      <div className="content">
-        <h2 className="content__title">{name}</h2>
-        <h3 className="content__company">{company}</h3>
-        <p className="content__info">{description}</p>
-        {
-         multipleButtons ? links.map( (button, index) => 
-            <a className={`content__cta ${button.class}`} key={index} href={button.link} target="_blank" rel="noreferrer noopener">View Work</a>
-          ) : 
-          <a className="content__cta" href={link} target="_blank" rel="noreferrer noopener">View Work</a>
-        }
-        <ul className="content__tags">
+    <Fade>
+      <div className={`${themeClass} portfolio grid-wrapper col-2 align-center grid-gap-4 my-3`}>
+        <div className="media">
+          <picture>
+            <source srcSet={imageWebP} type="image/webp" />
+            <source srcSet={image} type="image/png" />
+            <img className="img-fluid rounded media__image" src={image} alt={`${company}`} />
+          </picture>
+        </div>
+        <div className="content">
+          <h2 className="content__title">{name}</h2>
+          <h3 className="content__company">{company}</h3>
+          <p className="content__info">{description}</p>
           {
-            tags.map((tag, index) => {
-              return <li key={index}>{tag}</li>
-            })
+            multipleButtons ? links.map((button, index) =>
+              <a className={`content__cta ${button.class}`} key={index} href={button.link} target="_blank" rel="noreferrer noopener">View Work</a>
+            ) :
+              <a className="content__cta" href={link} target="_blank" rel="noreferrer noopener">View Work</a>
           }
-        </ul>
+          <ul className="content__tags">
+            {
+              tags.map((tag, index) => {
+                return <li key={index}>{tag}</li>
+              })
+            }
+          </ul>
+        </div>
       </div>
-
-    </div>
+    </Fade>
   )
 
 
--- src/layouts/Portfolio.js
+++ src/layouts/Portfolio.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 import portfolioData from '../data/portfolio';
 import PortfolioPanel from '../components/PortfolioPanel';
 
-// Libraries
-import Fade from 'react-reveal/Fade';
-
 
 class Portfolio extends Component {
 
@@ -20,24 +17,22 @@ class Portfolio extends Component {
 
   render() {
     return (
-      <Fade>
-        <section id="portfolio">
-          <div className="container py-5">
-            <h1>Portfolio</h1>
-            <p>Some of my latest work from my previous work experience and freelance projects</p>
-            <div className="portfolio-panel-container">
-              {
-                this.state.portfolioData.map((item, index) => {
-                  return (
-                    <PortfolioPanel key={index} portfolioData={item} />
-                  )
-                })
-              }
+      <section id="portfolio">
+        <div className="container py-5">
+          <h1>Portfolio</h1>
+          <p>Some of my latest work from my previous work experience and freelance projects</p>
+          <div className="portfolio-panel-container">
+            {
+              this.state.portfolioData.map((item, index) => {
+                return (
+                  <PortfolioPanel key={index} portfolioData={item} />
+                )
+              })
+            }
 
-            </div>
           </div>
-        </section>
-      </Fade>
+        </div>
+      </section>
     );
   }
 
