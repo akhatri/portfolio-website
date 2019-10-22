96162bba0de3b1b5f8e9608343f8bd40144c6057
Akshat Khatri <akshatster@gmail.com>
2019-10-22 19:36:42 +0100

    Update Contact.js

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -15,10 +15,10 @@ class Contact extends Component {
       <Fade>
         <section id="contact" className="bg-grey">
           <div className="container py-5">
+            <h1>Get in touch <span className="emoji" aria-label="Emoji heart letter" role="img">💌</span></h1>
             <div className="grid-wrapper col-2 align-center grid-gap-4">
               <img className="img-fluid rounded" src={ContactImage} alt="Get in touch" />
               <div>
-                <h1>Get in touch <span className="emoji" aria-label="Emoji heart letter" role="img">💌</span></h1>
                 <p>If you would like to get in touch regarding any freelance consulting projects and start-up advice on technology roadmaps, please get in touch via email <a href="mailto:akshatster@gmail.com">here</a></p>
                 <p>I'm currently based in UK and open to any contract roles too, depending on my work-load <span aria-label="Emoji smiley" role="img">😄</span></p>
               </div>
