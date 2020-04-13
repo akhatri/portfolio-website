0b228b6855b94a3b649d18e3ff0c5d85d616afda
akhatri <akshatster@gmail.com>
2020-04-14 00:21:15 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.4630592ff05cd3aeb39867fc575cc9fd.js"
+  "/portfolio-website/precache-manifest.0ea72279722df4270de266c11c6467c4.js"
 );
 
 self.addEventListener('message', (event) => {
